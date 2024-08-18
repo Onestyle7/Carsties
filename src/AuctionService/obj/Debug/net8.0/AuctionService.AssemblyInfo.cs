67b64449a178cadbd14b9bd719d1d7383e6e45b3
Bartłomiej Kłuś <bartlomiej.klus@microsoft.wsei.edu.pl>
2024-08-18 19:00:31 +0200

    Added Controller for auctions
    - Added ctor with dependecies to dbcontext and mapper
    - Added endpoint to get all Auctions from the site
    - Added endpoint to get specific auction from te site by id

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+778fadbf8a1dcee9ce5ce62e044df797d7a431c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e66749c2f1a60eca59265db55b6a3d64454640")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
