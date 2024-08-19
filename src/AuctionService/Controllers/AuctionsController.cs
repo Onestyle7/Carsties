d1ecc8c9f8d3ac5465d812928119f4ad6383232f
Bartłomiej Kłuś <bartlomiej.klus@microsoft.wsei.edu.pl>
2024-08-19 14:42:16 +0200

    Added DeleteAuction controller

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -81,4 +81,20 @@ public class AuctionsController : ControllerBase
         return BadRequest("Problem saving changes");
 
     }
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions
+            .FindAsync(id);
+        
+        if(auction == null) return NotFound();
+
+        //TODO: check seller == username
+        _context.Remove(auction);
+
+        var result = await _context.SaveChangesAsync() > 0; 
+        if(!result) return BadRequest("Could not update Db");
+
+        return Ok();
+    }
 }
