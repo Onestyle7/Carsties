971f0ca07c125d36012b91853232a06d88478043
Onestyle7 <klusbartek8@gmail.com>
2024-08-20 18:04:26 +0200

    - Added new model as Item - Added Connection string to MongoDb

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94f743fbb3c128c35320d4924145034324eb03d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+794e8a4673457b6fac4783d982bd694308da9716")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
