476ecdcf26de112e4cc487c6fcc7031ec4f80eef
Onestyle7 <klusbartek8@gmail.com>
2024-09-01 21:21:33 +0200

    - Publish AuctionUpdated event after auction update - Publish AuctionDeleted event after auction deletion

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -85,6 +85,8 @@ public class AuctionsController : ControllerBase
         auction.Item.Mileage = updateAuctionDto.Mileage ?? auction.Item.Mileage;
         auction.Item.Year = updateAuctionDto.Year ?? auction.Item.Year;
 
+        await _publishEndpoint.Publish(_mapper.Map<AuctionUpdated>(auction));
+
         var result = await _context.SaveChangesAsync() > 0;
 
         if(result) return Ok();
@@ -103,7 +105,10 @@ public class AuctionsController : ControllerBase
         //TODO: check seller == username
         _context.Remove(auction);
 
+        await _publishEndpoint.Publish<AuctionDeleted>(new{Id = auction.Id.ToString()});
+
         var result = await _context.SaveChangesAsync() > 0; 
+        
         if(!result) return BadRequest("Could not update Db");
 
         return Ok();
