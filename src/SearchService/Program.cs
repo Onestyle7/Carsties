825268e284a517e6f629836b9ea8bc901ee894ad
Onestyle7 <klusbartek8@gmail.com>
2024-09-01 18:34:29 +0200

    - Add retry policy and consumer configuration to search-auction-created endpoint in MassTransit

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,20 +1,20 @@
 using System.Net;
+using Contracts;
 using MassTransit;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using Polly;
 using Polly.Extensions.Http;
+using SearchService;
 using SearchService.Consumers;
-using SearchService.Data;
-using SearchService.Models;
-using SearchService.Services;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.AddHttpClient<AuctionSvcClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 builder.Services.AddMassTransit(x =>
 {
     x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
@@ -23,6 +23,12 @@ builder.Services.AddMassTransit(x =>
 
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.ReceiveEndpoint("search-auction-created", e =>{
+            e.UseMessageRetry(r => r.Interval(5, 5));
+
+            e.ConfigureConsumer<AuctionCreatedConsumer>(context);
+        });
+
         cfg.ConfigureEndpoints(context);
     });
 });
