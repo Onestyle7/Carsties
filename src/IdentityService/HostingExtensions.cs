affc0e9b6aa730d72c5cd0f6fa76b34eee85908b
Onestyle7 <klusbartek8@gmail.com>
2024-09-04 18:26:17 +0200

    - Added Connection String to default Db - Added Postgres nuget to project - Added new Migration for PostgressDb and deleted for SqlLite

--- src/IdentityService/HostingExtensions.cs
+++ src/IdentityService/HostingExtensions.cs
@@ -14,7 +14,7 @@ internal static class HostingExtensions
         builder.Services.AddRazorPages();
 
         builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
         builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
             .AddEntityFrameworkStores<ApplicationDbContext>()
@@ -35,7 +35,10 @@ internal static class HostingExtensions
             .AddInMemoryApiScopes(Config.ApiScopes)
             .AddInMemoryClients(Config.Clients)
             .AddAspNetIdentity<ApplicationUser>();
-        
+        builder.Services.ConfigureApplicationCookie(options =>{
+            options.Cookie.SameSite = SameSiteMode.Lax;
+            
+        });
         builder.Services.AddAuthentication();
 
         return builder.Build();
