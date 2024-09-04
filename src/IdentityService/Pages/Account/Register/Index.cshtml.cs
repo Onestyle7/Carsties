23a91dfd313e3d702a782859df7993f7136f534e
Onestyle7 <klusbartek8@gmail.com>
2024-09-04 18:58:34 +0200

    - Added Register Page with routing from Login page for new users

---
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -0,0 +1,19 @@
+using System.Security.Claims;
+using IdentityModel;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityService.Pages.Register
+{
+    [SecurityHeaders]
+    [AllowAnonymous]
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
