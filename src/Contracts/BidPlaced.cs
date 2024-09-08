368945af64d2fc1fba751ab761ed15f0db6d7c08
Onestyle7 <klusbartek8@gmail.com>
2024-09-08 20:09:40 +0200

    - Fixed missed property in BidPlaced

--- src/Contracts/BidPlaced.cs
+++ src/Contracts/BidPlaced.cs
@@ -8,5 +8,6 @@ public class BidPlaced
     public string AuctionId { get; set; }
     public string Bidder { get; set; }
     public DateTime BidTime { get; set; }
+    public int Amount { get; set; }
     public string BidStatus { get; set; }
 }
