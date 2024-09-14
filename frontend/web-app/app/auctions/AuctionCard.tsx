4125830c6ccf86aa3a17929939cf66c6b258a876
Onestyle7 <klusbartek8@gmail.com>
2024-09-14 13:53:22 +0200

    - Added AuctionCard component

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
+import Image from 'next/image'
+import CountdownTimer from './CountdownTimer'
 
 type Props = {
-    auction: any
-
-    
+    auction: any    
 }
 
 export default function AuctionCard({auction}: Props) {
   return (
-    <div>{auction.make}</div>
+    <a href='#'>
+      <div className='relative w-full bg-gray-200 aspect-[16/10] rounded-lg overflow-hidden'>
+        <Image 
+          src={auction.imageUrl}
+          alt={`Image of ${auction.make} ${auction.model} in ${auction.color}`}
+          fill
+          className='object-cover'
+          sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 25vw'
+        />
+        <div className='absolute bottom-2 left-2'>
+        <CountdownTimer auctionEnd={auction.auctionEnd} />
+        </div>
+      </div>
+      <div className='flex justify-between  items-center mt-4'>
+        <h3 className='text-gray-700'>{auction.make} {auction.model}</h3>
+        <p className='font-bold text-sm'>{auction.year}</p>
+      </div>
+
+    </a>
   )
 }
