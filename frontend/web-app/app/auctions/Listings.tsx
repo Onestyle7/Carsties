c366caa4874ca6fc25510896c39942b68b6fa10f
Onestyle7 <klusbartek8@gmail.com>
2024-09-14 17:06:41 +0200

    - Added Pagination component

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { json } from 'stream/consumers';
 import AuctionCard from './AuctionCard';
+import { Auction, PageResult } from '@/types';
+import AppPagination from '../components/AppPagination';
 
-async function getData(){
-    const res = await fetch('http://localhost:6001/search?pageSize=10');
+async function getData(): Promise<PageResult<Auction>>{
+    const res = await fetch('http://localhost:6001/search?pageSize=4');
 
     if(!res.ok) throw new Error('Failed to fetch data');
         
@@ -14,10 +16,16 @@ async function getData(){
 export default async function Listings() {
     const data = await getData();
     return (
+        <>
         <div className='grid grid-cols-4 gap-6'>
             {data && data.results.map((auction: any) =>(
                 <AuctionCard auction={auction} key={auction.id}/>
             ))}
         </div>
+        <div className='flex justify-center mt-4'>
+            <AppPagination currentPage={1} pageCount={data.pageCount}/>
+        </div>
+        </>
+        
     ) 
 }
