189e6b6999735baf83b2ae1f23b01af7b8a39e3f
Onestyle7 <klusbartek8@gmail.com>
2024-09-17 14:37:15 +0200

    - Update Filters comontent to use global state from useParamsStore

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import { ButtonGroup, Button } from 'flowbite-react'
+import { useParamsStore } from '@/hooks/useParamsStore';
 
-type Props ={
-    pageSize: number,
-    setPageSize: (size: number) => void,
-}
 
 const pageSizeButtons = [4, 8, 12]
 
-export default function Filters({pageSize, setPageSize}: Props) {
+export default function Filters() {
+    const pageSize = useParamsStore(state => state.pageSize);
+    const setParams = useParamsStore(state => state.setParams);
+
   return (
     <div className='flex justify-between items-center mb-4'>
         <div>
             <span className='uppercase text-sm text-gray-500 mr-2'>Page size</span>
             <ButtonGroup>
                 {pageSizeButtons.map((value, i) =>(
-                    <Button key={i} onClick={() =>
-                        setPageSize(value)}
+                    <Button key={i} 
+                        onClick={() => setParams({pageSize: value})}
                         color={`${pageSize === value ? 'red' : 'grey'}`}
                         className='focus:ring-0'
                         >
