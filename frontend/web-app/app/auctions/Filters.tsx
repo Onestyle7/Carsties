79206ee70fe0acacbf2fa7a5502d135693dd4dfa
Onestyle7 <klusbartek8@gmail.com>
2024-09-19 19:06:27 +0200

    Added filtering by: - Recently Added - End date - Alphabetical

--- frontend/web-app/app/auctions/Filters.tsx
+++ frontend/web-app/app/auctions/Filters.tsx
@@ -1,16 +1,54 @@
 import React from 'react'
 import { ButtonGroup, Button } from 'flowbite-react'
 import { useParamsStore } from '@/hooks/useParamsStore';
+import { AiOutlineClockCircle, AiOutlineSortAscending } from 'react-icons/ai';
+import { BsFillStopCircleFill } from 'react-icons/bs';
+import { IconBase } from 'react-icons';
 
 
-const pageSizeButtons = [4, 8, 12]
+const pageSizeButtons = [4, 8, 12];
+
+const orderButtons = [
+    {
+        label: 'Alphabetical',
+        icon: AiOutlineSortAscending,
+        value: 'make'
+    },
+    {
+        label: 'End date',
+        icon: AiOutlineClockCircle,
+        value: 'endingSoon'
+    },
+    {
+        label: 'Recently added',
+        icon: BsFillStopCircleFill,
+        value: 'new'
+    }
+]
 
 export default function Filters() {
     const pageSize = useParamsStore(state => state.pageSize);
     const setParams = useParamsStore(state => state.setParams);
+    const orderBy = useParamsStore(state => state.orderBy);
 
   return (
     <div className='flex justify-between items-center mb-4'>
+        <div>
+            <span className='uppercase text-sm text-gray-500 mr-2'>Order by</span>
+            <Button.Group>
+                {orderButtons.map(({label, icon: Icon, value })=> (
+                    <Button
+                        key={value}
+                        onClick={() => setParams({orderBy: value})}
+                        color={`${orderBy === value ? 'red': 'grey'}`}
+                    >
+                        <Icon className='mr-3 h-4 w-4' />
+                        {label}
+                    </Button> 
+                ))}
+            </Button.Group>
+        </div>
+
         <div>
             <span className='uppercase text-sm text-gray-500 mr-2'>Page size</span>
             <ButtonGroup>
