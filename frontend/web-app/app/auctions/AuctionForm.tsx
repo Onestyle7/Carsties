7305409707b1c567727bbb8801d3f7c58f7ad012
Onestyle7 <klusbartek8@gmail.com>
2024-09-30 18:34:17 +0200

    Add input form validation

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -4,9 +4,10 @@ import { error } from 'console';
 import { Button, TextInput } from 'flowbite-react';
 import React from 'react'
 import { FieldValues, Form, useForm } from 'react-hook-form';
+import Input from '../components/Input';
 
 export default function AuctionForm() {
-    const {register, handleSubmit, setFocus,
+    const {control, handleSubmit, setFocus,
         formState: {isSubmitting, isValid, isDirty, errors}} = useForm();
 
       function onSubmit(data: FieldValues){
@@ -15,22 +16,11 @@ export default function AuctionForm() {
 
     return (
       <form className='flex flex-col mt-3' onSubmit={handleSubmit(onSubmit)}>
-        <div className='mb-3 block'>
-          <TextInput 
-            {...register('make', {required: 'Make is required'})}
-            placeholder='Make'
-            color={errors?.make && 'failure'}
-            helperText={errors?.make?.message as string}
-          />
-        </div>
-        <div className='mb-3 block'>
-        <TextInput 
-            {...register('model', {required: 'Model is required'})}
-            placeholder='Model'
-            color={errors?.model && 'failure'}
-            helperText={errors?.model?.message as string}
-          />
-        </div>
+        <Input label='Make' name='make' control={control} 
+          rules={{required: "Make is required!"}}/>
+        <Input label='Model' name='model' control={control} 
+          rules={{required: "Model is required!"}}/>
+
         <div className='flex justify-between'>
           <Button outline color='gray'>Cancel</Button>
           <Button 
