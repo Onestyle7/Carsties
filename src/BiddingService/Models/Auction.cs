d6360bbf4093c3d3a8ace3aa335a84b66a1e6ba2
Onestyle7 <klusbartek8@gmail.com>
2024-10-03 21:57:38 +0200

    Add BidsController with PlaceBid endpoint for bidding functionality
    - Implement PLaceBid action to handle bid placement on auctions
    - Validate auction existence and ownership
    - Prevent users from bidding on their own auctions
    - Determine BidStatus based on bid amount and auction conditions
    - Save the bid to the database
    - Terun the bid details in the response

--- src/BiddingService/Models/Auction.cs
+++ src/BiddingService/Models/Auction.cs
@@ -1,12 +1,13 @@
 using System;
+using MongoDB.Entities;
 
 namespace BiddingService.Models;
 
-public class Auction
+public class Auction : Entity
 {
     public DateTime AuctionEnd { get; set; }
     public string Seller { get; set; }
     public int ReservePrice { get; set; }
     public bool Finished { get; set; }
-    
+
 }
