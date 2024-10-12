781ed88c29aac64ba63a9132cec13a03c46e1304
Onestyle7 <klusbartek8@gmail.com>
2024-10-12 21:08:38 +0200

    fixed error with compilation -532462766

--- src/NotificationService/Program.cs
+++ src/NotificationService/Program.cs
@@ -2,15 +2,22 @@ using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var rabbitMqHost = builder.Configuration.GetValue<string>("RabbitMq:Host", "localhost");
+var rabbitMqUsername = builder.Configuration.GetValue<string>("RabbitMq:Username", "guest");
+var rabbitMqPassword = builder.Configuration.GetValue<string>("RabbitMq:Password", "guest");
+
+Console.WriteLine($"RabbitMQ Host: {rabbitMqHost}");
+Console.WriteLine($"RabbitMQ Username: {rabbitMqUsername}");
+
 builder.Services.AddMassTransit(x => 
 {
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
 
     x.UsingRabbitMq((context, cfg) => 
     {
-        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>{
-            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
-            host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        cfg.Host(rabbitMqHost, "/", host => {
+            host.Username(rabbitMqUsername);
+            host.Password(rabbitMqPassword);
         });
 
         cfg.ConfigureEndpoints(context);
