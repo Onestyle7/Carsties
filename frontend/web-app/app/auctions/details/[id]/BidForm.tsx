d4eaa8c7e9d0559f944a432eca03e2338451b2b0
Onestyle7 <klusbartek8@gmail.com>
2024-10-14 13:51:33 +0200

    Improve error handling and bid sumbission in BidForm compontent
    - Added proper error handling for bid submisstion
    - Ensured bid is only added after successful response
    - Improved form reset logic post bid submistion

--- frontend/web-app/app/auctions/details/[id]/BidForm.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidForm.tsx
@@ -11,18 +11,22 @@ import { numberWithCommas } from '@/app/lib/numberWithComma';
 import { useBidStore } from '@/hooks/useBidStore';
 import React from 'react'
 import { FieldValues, useForm } from 'react-hook-form';
+import toast from 'react-hot-toast';
 
 export default function BidForm({auctionId, highBid}: Props) {
   const {register, handleSubmit, reset, formState: {errors}} = useForm();
   const addBid = useBidStore(state => state.addBid);
   
   function onSubmit(data: FieldValues){
-    placeBidForAuction(auctionId, +data.amount).then(bid =>{
-        addBid(bid);
-        reset();
-    })  
-
-}
+    placeBidForAuction(auctionId, +data.amount).then(bid => {
+        if (bid.error) {
+            throw bid.error;
+        } else {
+            addBid(bid);
+            reset();
+        }
+    }).catch(err => toast.error(err.message));
+  }
   return (
     <form onSubmit={handleSubmit(onSubmit)} className='flex items-center border-2 roundend-lg py-2'>
         <input 
