16b8f53109ed5562139498e64b691e55d0961b82
Onestyle7 <klusbartek8@gmail.com>
2024-10-14 13:33:51 +0200

    Update BidList compontent to display highest bid and improve UI
    - Added high bid display with formatted numbers
    - Refined UI elements for bid listing

--- frontend/web-app/app/auctions/details/[id]/BidList.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidList.tsx
@@ -8,6 +8,9 @@ import { User } from 'next-auth'
 import React, { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import BidItem from './BidItem'
+import { numberWithCommas } from '@/app/lib/numberWithComma'
+import EmptyFilter from '@/app/components/EmptyFilter'
+import BidForm from './BidForm'
 
 type Props ={
   user: User | null,
@@ -19,6 +22,8 @@ export default function BidList({user, auction}: Props) {
   const [loading, setLoading] = useState(true)
   const bids = useBidStore(state => state.bids)
   const setBids = useBidStore(state => state.setBids)
+
+  const highBid = bids.reduce((prev, current) => prev > current.amount ? prev : current.amount, 0)
 
   useEffect(()=> {
     getBidsForAuction(auction.id)
@@ -35,11 +40,27 @@ export default function BidList({user, auction}: Props) {
   if(loading) return <span>Loading bids...</span>
 
   return (
-    <div className='border-2 rounded-lg p-2 bg-gray-100'>
-    <Heading title='Bids' subtitle={''} />
-    {bids.map(bid => (
-      <BidItem key={bid.id} bid={bid}/>
-    ))}
+    <div className='rounded-lg shadow-md'>
+      <div className='py-2 px-4 bg-white'>
+        <div className='sticky top-0 bg-white p-2'>
+          <Heading title={`Current high bid is ${numberWithCommas(highBid)}`} subtitle='Please fell free to make a bid'/>
+        </div>
+      </div>
+
+      <div className='overflow-auto h-[400px] flex flex-col-reverse px-2'>
+        {bids.length === 0? (
+          <EmptyFilter title='No bids for this item' subtitle='Please fell free to make a bid' />
+        ): (
+          <>
+            {bids.map(bid => (
+              <BidItem key={bid.id} bid={bid}/>
+            ))}
+          </>
+        )}
+      </div>
+    <div className='px-2 pb-2 text-grey-500'>
+        <BidForm auctionId={auction.id} highBid={highBid}/>
+    </div>
   </div>
   )
 }
